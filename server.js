1a018119975c7166d8b0d8a3500de2d9257d8f53
dsivatgl <sivananth56666@gmail.com>
2021-12-07 01:08:02 +0530

    second heroku changes

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/sivananth-color'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/sivananth-color/index.html'));});
-app.listen(process.env.PORT || 8080);
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '/dist')));
+
+app.get('/*', function(req, res) {
+  res.
+
+sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(8080, function () {
+  console.log('App started');
+});
